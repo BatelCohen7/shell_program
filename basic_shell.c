5cc8efb56893125f825ba38748c906d54df7fd75
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-04-30 18:36:55 +0300

    Add files via upload

--- basic_shell.c
+++ basic_shell.c
@@ -7,138 +7,278 @@
 #include "unistd.h"
 #include <string.h>
 #include <ctype.h>
+#include <signal.h>
 
-// Part A:
-int cmp(char *file1, char *file2, int verbose, int ignoreCase)
-{
-	FILE *f1, *f2;
-	char ch1, ch2;
-	int result = 0;
+// int main() {
+// 	int i;
+// 	char *argv[10];
+// 	char command[1024];
+// 	char *token;
+	
+// 	printf("--------------Welcome to stshell--------------\n");
+	
+// 	while (1) {
+// 	    printf("\ncommand: ");
+// 	    fgets(command, 1024, stdin);
+// 	    command[strlen(command) - 1] = '\0'; // replace \n with \0
 
-	f1 = fopen(file1, "r");
-	f2 = fopen(file2, "r");
-	// i need to chack if use "r" or "rb" if the compare between two file text or all the types
+// 	    /* parse command line */
+// 	    i = 0;
+// 	    token = strtok (command," ");
+// 	    while (token != NULL)
+// 	    {
+// 		argv[i] = token;
+// 		token = strtok (NULL, " ");
+// 		i++;
+// 	    }
+// 	    argv[i] = NULL;
 
-	if (f1 == NULL || f2 == NULL)
-	{
-		fprintf(stderr, "Error opening one of the files: %s\n", strerror(errno));
-		goto cleanup;
-	}
+// 	    if (argv[0] != NULL){
 
-	while (1)
-	{
-		ch1 = fgetc(f1);
-		ch2 = fgetc(f2);
-
-		// we chack if finish to read the file
-		if (ch1 == EOF || ch2 == EOF)
-		{
-			break;
-		}
-
-		if (ignoreCase)
-		{
-			ch1 = tolower(ch1);
-			ch2 = tolower(ch2);
-		}
-
-		if (ch1 != ch2)
-		{
-			result = 1;
-			if(verbose){printf("thw files are distinct");}
-			return result;
-		}
-	}
-	if(verbose)
-	{
-		printf("thw files are equal");
-	}
+// 		}
+// 	}
+	
+// 	pid_t pid = fork();  // create a new child process
+//     if (pid == -1) {
+//         perror("fork function filed");  // handle error condition
+//         exit(EXIT_FAILURE);
+//     }
+	
+// 	if (pid == 0) {
+//         printf("Child process: PID=%d\n", getpid());
+        
+//     } else {
+//         printf("Parent process: PID=%d, child PID=%d\n", getpid(), pid);
+//         wait(NULL);
+//     }
+// 	return 0;
+// }
 
 
-cleanup:
-	if (f1 != NULL)
-	{
-		fclose(f1);
-	}
-	if (f2 != NULL)
-	{
-		fclose(f2);
-	}
-	return result;
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <netinet/in.h> //structure for storing address information
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
+#include <fcntl.h>
+
+#define TRUE 1
+#define MAX_CMD_SIZE 1024
+
+void myPipe(char cmd[MAX_CMD_SIZE]){
+    char *tok1;
+    char *tok2; 
+    tok2 = strtok(cmd, "|");
+    tok1 = tok2;
+    tok2 = strtok(NULL, "|");
+
+    int fd[2];
+    if (pipe(fd) == -1){
+        return ;
+    }
+    int pid1 = fork();
+    if (pid1 < 0)
+    {
+        return ;
+    }
+    if (pid1 == 0)
+    {
+        // we stand on the left process child
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        char *token;
+        token = strtok(tok1, " ");
+        char *param[1024];
+        int i = 0;
+        while (token != NULL)
+        {
+            param[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        param[i] = NULL;
+        printf("%s", param[0]);
+        if (execvp(param[0], param) == -1)
+        {
+            printf("error1");
+            return ;
+        }
+    }
+
+    int pid2 = fork();
+    if (pid2 < 0)
+    {
+        return ;
+    }
+    if (pid2 == 0)
+    {
+        // we stand on the right process child
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        char *token;
+        token = strtok(tok2, " ");
+        char *param[1024];
+        int i = 0;
+        while (token != NULL)
+        {
+            param[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        param[i] = NULL;
+        if (execvp(param[0], param) == -1)
+        {
+            printf("error");
+            return ;
+        }
+    }
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    close(fd[0]);
+    close(fd[1]);
 }
 
-int copy(int verbos, int force, char *src_file_path, char *dest_file_path){
-	FILE *src_file = NULL;
-	FILE *dest_file = NULL;
-	int result = 0;
-	src_file = fopen(src_file_path,"rb");
-	if(src_file == NULL){
-		fprintf(stderr,"Error opening source file:%s\n",strerror(errno));
-		result = 1;
-		goto cleanup;
-	}
-	dest_file = fopen(dest_file_path, force?"wb":"rb");
-	if(dest_file == NULL){
-		if(errno == EEXIST && !(force)){
-			fprintf(stderr,"Target file already exist\n");
-			result = 1;
-			goto cleanup;
-		}else{
-			fprintf(stderr,"Error opening file:%s",strerror(errno));
-			result = 1;
-			goto cleanup;
+int main(){
+
+	printf("--------------Welcome to stshell--------------\n");
+
+    char cmd[MAX_CMD_SIZE] = "";
+	const char *ctrl_c_str = "\x03";
+	const char *redirect_str1 = ">>";
+	const char *redirect_str2 = "<<";
+
+    while (TRUE){
+		printf("\ncommand: ");
+        fgets(cmd, MAX_CMD_SIZE, stdin);
+        cmd[strlen(cmd) - 1] = '\0';
+
+		if(strcmp(cmd, "exit") == 0){
+			printf("\n--------------close to stshell--------------\n");
+			exit(EXIT_SUCCESS);
 		}
-	}
-	int c;
-	while(((c = fgetc(src_file)) != EOF)){
-		fputc(c, dest_file);
-	}
-	if (verbos){
-		printf("Success\n");
-	}
+		else if(strstr(cmd, ctrl_c_str) != NULL){
+			int pid = fork();
+			if(pid == -1){
+				perror("fork function filed\n");  // handle error condition
+        		exit(EXIT_FAILURE);
+			}
+			if(pid == 0){
+				int result = kill(getppid(), SIGSTOP);
+				if (result == -1) {
+        			perror("kill function filed\n");
+        			return 1;
+    			}
+				printf("to continue the program pleas press 1\n");
+				int flag;
+				while(TRUE){
+					scanf("%d", &flag);
+					if (flag == 1){
+						int result = kill(getppid(), SIGCONT);
+						if (result == -1){
+        					perror("kill function filed\n");
+        					return 1;
+    					}
+					}
+				}
+			}
+			else{
+				sleep(1);
+			}
+		}
+        else if (strchr(cmd, '|') != NULL){
+            myPipe(cmd);
+        }
+		else if ((strchr(cmd, '>') != NULL) || (strchr(cmd, '<') != NULL)||
+				 (strstr(cmd, redirect_str1) != NULL) || (strstr(cmd, redirect_str2) != NULL)){
 
-	cleanup:
-	if (src_file != NULL)
-	{
-		fclose(src_file);
-	}
-	if (dest_file != NULL)
-	{
-		fclose(dest_file);
-	}
-	return result;
+        		char *tok1; // process
+            	char *tok2; // file
+				if (strchr(cmd, '<') != NULL){
+                	tok2 = strtok(cmd, "<");
+                	tok1 = tok2;
+                	tok2 = strtok(NULL, "<");
+            	}else if(strstr(cmd, redirect_str2) != NULL){
+					tok2 = strtok(cmd, redirect_str2);
+                	tok1 = tok2;
+                	tok2 = strtok(NULL, redirect_str2);
+				}else if(strstr(cmd, redirect_str1) != NULL){
+					tok2 = strtok(cmd, redirect_str1);
+                	tok1 = tok2;
+                	tok2 = strtok(NULL, redirect_str1);
+				}
+				else if (strchr(cmd, '>') != NULL){
+                	tok2 = strtok(cmd, ">");
+                	tok1 = tok2;
+                	tok2 = strtok(NULL, ">");
+            	}
+
+            	int pid = fork();
+            	if (pid == -1) {
+        			perror("fork function filed\n");  // handle error condition
+        			exit(EXIT_FAILURE);
+    			}
+            	if (pid == 0){
+					if (tok2[0] == ' '){
+						tok2++;
+					}
+					int file = open(tok2, O_WRONLY | O_CREAT, 0777);
+					if (file == -1){
+						printf("open file function filed\n");
+						return 2;
+					}
+					dup2(file, STDOUT_FILENO);
+					close(file);
+						
+					char *token;
+					token = strtok(tok1, " ");
+					char *argv[MAX_CMD_SIZE];
+					int i = 0;
+					while (token != NULL){
+						argv[i] = token;
+						token = strtok(NULL, " ");
+						i++;
+					}
+					argv[i] = NULL;
+
+					// test
+					printf("%s", argv[0]);
+
+					if (execvp(argv[0], argv) == -1){
+						perror("execute function filed\n");  
+        				return 1;
+					}
+				}
+		}
+        else{
+        	int pid = fork();
+        	if (pid == -1){
+            	perror("fork function filed\n");  
+        		exit(EXIT_FAILURE);
+        	}
+        	if (pid == 0){
+				char *token;
+				token = strtok(cmd, " ");
+				char *argv[1024];
+				int i = 0;
+				while (token != NULL){
+					argv[i++] = token;
+					token = strtok(NULL, " ");
+				}
+				argv[i] = NULL;
+				if (execvp(argv[0], argv) == -1){
+					printf("execvp function filed\n");
+					return 1;
+				}
+        	}
+        }
+    }
+    return 0;
 }
 
-int main() {
-	int i;
-	char *argv[10];
-	char command[1024];
-	char *token;
 
-	while (1) {
-	    printf("hello: ");
-	    fgets(command, 1024, stdin);
-	    command[strlen(command) - 1] = '\0'; // replace \n with \0
-
-	    /* parse command line */
-	    i = 0;
-	    token = strtok (command," ");
-	    while (token != NULL)
-	    {
-		argv[i] = token;
-		token = strtok (NULL, " ");
-		i++;
-	    }
-	    argv[i] = NULL;
-
-	    /* Is command empty */
-	    if (argv[0] == NULL)
-		continue;
-
-	    /* for commands not part of the shell command language */
-	    if (fork() == 0) {
-		execvp(argv[0], argv);
-		wait(NULL);
-	    }
-	}
-}
